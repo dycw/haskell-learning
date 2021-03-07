1a900361615d2b79aae96220a1550256568768e3
Derek Wan <d.wan@icloud.com>
2021-03-07 17:23:32 +0800

    Stash

--- countdown.hs
+++ countdown.hs
@@ -21,5 +21,23 @@ apply Div x y = x `div` y
 data Expr = Val Int | App Op Expr Expr
 
 instance Show Expr where
-  show (Val n) = show n
-  show (Val n) = show n
+  show (Val n    ) = show n
+  show (App o l r) = brak l ++ show o ++ brak r
+   where
+    brak (Val n) = show n
+    brak e       = "(" ++ show e ++ ")"
+
+-- subs [1..3] == [[],[3],[2],[2,3],[1],[1,3],[1,2],[1,2,3]]
+subs :: [a] -> [[a]]
+subs []       = [[]]
+subs (x : xs) = yss ++ map (x :) yss where yss = subs xs
+
+-- interleave 1 [2..4] == [[1,2,3,4],[2,1,3,4],[2,3,1,4],[2,3,4,1]]
+interleave :: a -> [a] -> [[a]]
+interleave x []       = [[x]]
+interleave x (y : ys) = (x : y : ys) : map (y :) (interleave x ys)
+
+-- perms
+perms :: [a] -> [[a]]
+perms []       = [[]]
+perms (x : xs) = concat (map (interleave x) (perms xs))
